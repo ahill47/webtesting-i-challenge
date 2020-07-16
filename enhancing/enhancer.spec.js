c6c11729a9b82052b5bef80ec5c136e31d367a81
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-07-16 19:15:04 -0400

    finished

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,29 +1,59 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer");
 // test away!
-test ("runs our first test", ()=>{
-    expect(1+1).toBe(2)
-})
+describe("enhancer", () => {
+	describe("success()", () => {
+		it("should increase enhancement by one", () => {
+			expect(
+				enhancer.success({ name: "Bordeaux Axe", durability: 100, enhancement: 15 })
+			).toEqual({ name: "Bordeaux Axe", durability: 100, enhancement: 16 });
+		});
 
-describe ("enchancer", ()=>{
-    describe("repair()", ()=>{
-        it("should set durability to 100", ()=>{
-            expect(
-                enhancer.repair({name:"Alexis Hill", durability: 90, enhancement: 16})
-            ).toEqual({name:"Alexis Hill",durability: 100, enhancement: 16 })
-        })
-        it ("should stay at 100 if full", ()=>{
-            expect(
-                enhancer.repair({name:"Alexis Hill", durability: 90, enhancement: 16})
-            ).toEqual({name:"Alexis Hill",durability: 100, enhancement: 16 })
-        });
-    });// passed
+		it("should not increase enhancement if it is already 20", () => {
+			expect(
+				enhancer.success({ name: "Bordeaux Axe", durability: 100, enhancement: 20 })
+			).toEqual({ name: "Bordeaux Axe", durability: 100, enhancement: 20 });
+		});
+	});
+
+	describe("fail()", () => {
+		it("should decrease durability by 5 if enhancement < 15", () => {
+			expect(
+				enhancer.fail({ name: "Bordeaux Axe", durability: 100, enhancement: 10 })
+			).toEqual({ name: "Bordeaux Axe", durability: 95, enhancement: 10 });
+		});
+
+		it("should decrease durability by 10 if enhancement = 15", () => {
+			expect(
+				enhancer.fail({ name: "Bordeaux Axe", durability: 100, enhancement: 15 })
+			).toEqual({ name: "Bordeaux Axe", durability: 90, enhancement: 15 });
+		});
+
+		it("should decrease durability by 10 if enhancement > 15", () => {
+			expect(
+				enhancer.fail({ name: "Bordeaux Axe", durability: 100, enhancement: 16 })
+			).toEqual({ name: "Bordeaux Axe", durability: 90, enhancement: 16 });
+		});
+
+		it("should decrease durability by 10 and enhancement by 1 if enhancement > 16", () => {
+			expect(
+				enhancer.fail({ name: "Bordeaux Axe", durability: 100, enhancement: 17 })
+			).toEqual({ name: "Bordeaux Axe", durability: 90, enhancement: 16 });
+		});
+	});
+
+	describe("repair()", () => {
+		it("should set durability to 100", () => {
+			expect(
+				enhancer.repair({ name: "Bordeaux Axe", durability: 90, enhancement: 16 })
+			).toEqual({ name: "Bordeaux Axe", durability: 100, enhancement: 16 });
+		});
+
+		it("durability should stay at 100 if full", () => {
+			expect(
+				enhancer.repair({ name: "Bordeaux Axe", durability: 100, enhancement: 16 })
+			).toEqual({ name: "Bordeaux Axe", durability: 100, enhancement: 16 });
+		});
+	});
 
 
-
-
-
-
-
-
-
-})
+});
