a7b73c9425be00fc2c07f45632972d86f3e10689
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-07-16 17:39:09 -0400

    working on ttest

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,29 @@
 const enhancer = require('./enhancer.js');
 // test away!
+test ("runs our first test", ()=>{
+    expect(1+1).toBe(2)
+})
+
+describe ("enchancer", ()=>{
+    describe("repair()", ()=>{
+        it("should set durability to 100", ()=>{
+            expect(
+                enhancer.repair({name:"Alexis Hill", durability: 90, enhancement: 16})
+            ).toEqual({name:"Alexis Hill",durability: 100, enhancement: 16 })
+        })
+        it ("should stay at 100 if full", ()=>{
+            expect(
+                enhancer.repair({name:"Alexis Hill", durability: 90, enhancement: 16})
+            ).toEqual({name:"Alexis Hill",durability: 100, enhancement: 16 })
+        });
+    });// passed
+
+
+
+
+
+
+
+
+
+})
